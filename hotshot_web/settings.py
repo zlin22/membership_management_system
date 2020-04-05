80c006ad4991c63ddc4beb50ac99ad013b969ebf
zlin22 <zhi.long.lin@gmail.com>
2020-04-05 03:05:25 -0400

    static

--- hotshot_web/settings.py
+++ hotshot_web/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'admin'
 LOGOUT_REDIRECT_URL = '/admin'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
