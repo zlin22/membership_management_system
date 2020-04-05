9396a76e802ee94cadaef109164e6f3574960285
zlin22 <zhi.long.lin@gmail.com>
2020-04-05 03:27:27 -0400

    django heroku install

--- hotshot_web/settings.py
+++ hotshot_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,5 @@ LOGOUT_REDIRECT_URL = '/admin'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+django_heroku.settings(locals())
