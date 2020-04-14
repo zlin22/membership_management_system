0974fd336c271f1666654a9f5936c4e29be04a53
Zhi Lin <zhi.long.lin@gmail.com>
2020-04-14 03:15:29 -0400

    settings db postgres

--- hotshot_web/settings.py
+++ hotshot_web/settings.py
@@ -131,10 +131,21 @@ WSGI_APPLICATION = 'hotshot_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "zhilin",
+        "USER": "zhilin",
+        "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
