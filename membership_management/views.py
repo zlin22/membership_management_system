311046dd6f907f4c5fbd26661eacb95f0c1d78b4
zlin22 <zhi.long.lin@gmail.com>
2020-06-19 16:55:42 -0400

    fixed typo message2

--- membership_management/views.py
+++ membership_management/views.py
@@ -72,7 +72,7 @@ def check_in(request):
         if is_membership_active:
             context = {
                 "message1": f"{member.first_name} {member.last_name}!",
-                "message2": f"Your currently have an active membership",
+                "message2": f"You currently have an active membership",
                 "message3": f"It expires on {membership_expiration}",
                 "profile_pic": member.profile_pic,
                 "alert_type": "alert-success",
